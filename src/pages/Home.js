f325e7ac9d0244ea4cbe86034259d3e9c451c0b7
Farhan Sultan <farhansultan008@gmail.com>
2025-04-30 22:31:41 +0500

    update navbar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,14 +4,12 @@ import ProductList from "../features/product/components/ProductList";
 import Footer from "../features/common/Footer";
 
 function Home() {
-    return ( 
-        <div>
-            <NavBar>
-                <ProductList></ProductList>
-            </NavBar>
-            <Footer></Footer>
-        </div>
-     );
+  return (
+    <div>
+      <NavBar>{/* <ProductList></ProductList> */}</NavBar>
+      <Footer></Footer>
+    </div>
+  );
 }
 
 export default Home;

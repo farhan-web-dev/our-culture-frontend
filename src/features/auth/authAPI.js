957b64d48faf2a103bb83da029d97b9348a6cf8e
Farhan Sultan <farhansultan008@gmail.com>
2025-04-30 20:00:56 +0500

    update login

--- src/features/auth/authAPI.js
+++ src/features/auth/authAPI.js
@@ -13,12 +13,15 @@ export function createUser(userData) {
 export function loginUser(loginInfo) {
   return new Promise(async (resolve, reject) => {
     try {
-      const response = await fetch("/auth/login", {
-        method: "POST",
-        body: JSON.stringify(loginInfo),
-        headers: { "content-type": "application/json" },
-        credentials: "include",
-      });
+      const response = await fetch(
+        `${process.env.REACT_APP_BACKEND_URL}/auth/login`,
+        {
+          method: "POST",
+          body: JSON.stringify(loginInfo),
+          headers: { "content-type": "application/json" },
+          credentials: "include",
+        }
+      );
       if (response.ok) {
         const data = await response.json();
         resolve({ data });
